b6cf296d9979baf09cbc993f591b0eb77cc2ad11
Be3pReDeL <bogatovsergey111@gmail.com>
2024-01-08 15:05:05 +0300

    Migrated to PostgreSQL

--- .venv/bin/budgetbuddy/budgetbuddy/settings.py
+++ .venv/bin/budgetbuddy/budgetbuddy/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'budgetbuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'userDB',
+        'USER': 'postgres',
+        'PASSWORD': "password",
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
